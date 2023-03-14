4d932dccec13000df0d460222e9fb93c229b703d
xsm <xingshimeng@fancydigital.com.cn>
2023-03-14 10:10:17 +0800

    a

--- ZhApplication/app/src/main/java/com/example/zhapplication/MainActivity.java
+++ ZhApplication/app/src/main/java/com/example/zhapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.zhapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.optionmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        getMenuInflater().inflate(R.menu.optionmenu, menu);
+        return true;
     }
 }
